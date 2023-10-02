f25edbf6b9119044d912d4ec43c339248f0fa304
pyalamala <81705504+pyalamala@users.noreply.github.com>
2023-10-02 00:35:38 -0400

    completed us01 and us02 and added test files

--- gedcom/Features/sprint1.py
+++ gedcom/Features/sprint1.py
@@ -1,6 +1,7 @@
 from gedcom.gedcom_parse import get_gedcom_df
 import pandas as pd
 from gedcom.Features.utils import compare_dates
+import datetime
 
 
 import os
@@ -11,9 +12,46 @@ individuals, families = get_gedcom_df(f'{cwd}/gedcom/example.ged')
 
 
 # US01: Dates before current date
+def check_dates_before_current(individuals, families):
+    current_datetime = datetime.date.today()
+    allErrors = []
+
+    for _, row in individuals.iterrows():
+        if not pd.isna(row['BIRT']) and row['BIRT'] != 'NA':
+            if compare_dates(row['BIRT'], current_datetime.strftime("%d %b %Y")) == 1:
+                allErrors.append(f"ERROR: INDIVIDUAL: US01: {row['ID']}: Birthday {row['BIRT']} occurs in the future")
+        if not pd.isna(row['DEAT']) and row['DEAT'] != 'NA':
+            if compare_dates(row['DEAT'], current_datetime.strftime("%d %b %Y")) == 1:
+                allErrors.append(f"ERROR: INDIVIDUAL: US01: {row['ID']}: Death {row['DEAT']} occurs in the future")
+
+    for _, row in families.iterrows():
+        if not pd.isna(row['MARR']) and row['MARR'] != 'NA':
+            if compare_dates(row['MARR'], current_datetime.strftime("%d %b %Y")) == 1:
+                allErrors.append(f"ERROR: FAMILY: US01: {row['ID']}: Marriage {row['MARR']} occurs in the future")
+
+        if not pd.isna(row['DIV']) and row['DIV'] != 'NA':
+            if compare_dates(row['DIV'], current_datetime.strftime("%d %b %Y")) == 1:
+                allErrors.append(f"ERROR: FAMILY: US01: {row['ID']}: Divorce {row['DIV']} occurs in the future")
+
+    return allErrors
 
 # US02: Birth before marriage
+def check_birth_before_marriage(individuals, families):
+    allErrors = []
 
+    for _, row in families.iterrows():
+        if not pd.isna(row['MARR']) and row['MARR'] != 'NA':
+            husband_birth = (individuals[individuals['ID'] == row['HUSB']]['BIRT']).values
+            wife_birth = (individuals[individuals['ID'] == row['WIFE']]['BIRT']).values
+            if len(husband_birth) > 0 and len(wife_birth) > 0:
+                husband_birth = str(husband_birth[0])
+                wife_birth = str(wife_birth[0])
+                if compare_dates(husband_birth, row['MARR']) == 1:
+                    allErrors.append(f"ERROR: FAMILY: US02: {row['ID']}: Husband's ({row['HUSB']}) marriage date {row['MARR']} occurs before his birth date {husband_birth}")
+                if compare_dates(wife_birth, row['MARR']) == 1:
+                    allErrors.append(f"ERROR: FAMILY: US02: {row['ID']}: Wife's ({row['WIFE']}) marriage date {row['MARR']} occurs before her birth date {wife_birth}")
+
+    return allErrors
 
 # US03: Birth before death
 def check_birth_before_death(individuals):
@@ -112,7 +150,8 @@ def check_divorce_before_death(individuals, family):
                 if wife_errors: allErrors.append(wife_errors)
 
     return allErrors
-
+print(check_dates_before_current(individuals, families))
+print(check_birth_before_marriage(individuals, families))
 print(check_birth_before_death(individuals))
 print(check_marriage_before_divorce(families))
 print(check_marriage_before_death(individuals, families))
